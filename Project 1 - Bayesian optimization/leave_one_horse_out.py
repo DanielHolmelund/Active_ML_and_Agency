b07ce3faa7c4469fb5efd044cad6dfeda0810a49
Ne0-1 <57833596+Ne0-1@users.noreply.github.com>
2021-02-25 15:22:28 +0100

    Leave one horse out done <3

--- Project 1 - Bayesian optimization/leave_one_horse_out.py
+++ Project 1 - Bayesian optimization/leave_one_horse_out.py
@@ -1,33 +1,8 @@
 import pandas as pd
-import numpy as np
-import csv
-
-#Read dataset
-df = pd.read_csv("wasall_02445.txt", sep="	")
-
-#Create a dictionary and group by horse
-data_sets = {}
-by_class = df.groupby('horse')
-for groups, data in by_class:
-    data_sets[groups] = data
-
-#Create loop with test and train sets;
-for i in (np.unique(df["horse"])):
-    test = data_sets[i]
-
-    train = {}
-    for key, value in data_sets.items():
-        if key not in i:
-            train[key] = data_sets[key]
-
-    #Random Forest model here;
-
-with open('train.csv', 'w') as f:  # You will need 'wb' mode in Python 2.x
-    w = csv.DictWriter(f, train.keys())
-    w.writeheader()
-    w.writerow(train)
-
-with open('test.csv', 'w') as f:  # You will need 'wb' mode in Python 2.x
-    w = csv.DictWriter(f, train.keys())
-    w.writeheader()
-    w.writerow(train)
+#Split into test and train data
+def split_by_horse(df,horse):
+    test = df[df['horse'].str.match(horse)]
+    train = df.drop(df[df['horse'].str.match(horse)].index)
+    test = test.reset_index(drop=True)
+    train = train.reset_index(drop=True)
+    return test, train
