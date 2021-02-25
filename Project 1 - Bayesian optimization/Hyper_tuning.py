7361b9ddeafbee7776ae3e1f83861786b4a1d0e0
Yeganeh1377 <59656248+Yeganeh1377@users.noreply.github.com>
2021-02-25 14:17:08 +0100

    version2

--- Project 1 - Bayesian optimization/Hyper_tuning.py
+++ Project 1 - Bayesian optimization/Hyper_tuning.py
@@ -11,8 +11,8 @@ from scipy.stats import uniform
 from sklearn.utils import shuffle
 import random
 #Setting seed
-np.random.seed(34) #Seed 34 works
-random.seed(34)
+np.random.seed(2) #Seed 34 works
+random.seed(2)
 
 ####################Download data and prepping data####################
 filename = "wasall_02445.txt"
@@ -44,7 +44,7 @@ domain = {"n_estimators": range(1, 101),
 # rs.fit(Xtrain, ytrain)
 
 # create the ParameterSampler
-param_list = list(ParameterSampler(domain, n_iter=100, random_state=32))
+param_list = list(ParameterSampler(domain, n_iter=30, random_state=32))
 print('Param list')
 print(param_list)
 # rounded_list = [dict((k,v) for (k, v) in d.items()) for d in param_list]
@@ -131,11 +131,10 @@ def objective_function(x):
 opt = GPyOpt.methods.BayesianOptimization(f=objective_function,  # function to optimize
                                           domain=domain,  # box-constrains of the problem
                                           acquisition_type='EI',  # Select acquisition function MPI, EI, LCB
-                                          de_duplication=True)
+                                          )
 opt.acquisition.exploration_weight = 0.5
 
-opt.run_optimization(max_iter=95, eps=1e-8)
-
+opt.run_optimization(max_iter=25)
 
 x_best = opt.X[np.argmin(opt.Y)]
 print("The best parameters obtained: n_estimators=" + str(x_best[0]) + ", max_depth=" + str(
@@ -151,11 +150,10 @@ y_bo = np.maximum.accumulate(-opt.Y).ravel()
 # define iteration number
 xs = range(0,len(y_bo))
 
-plt.plot(xs, max_oob_per_iteration, 'o-', color = 'red', label='Random Search')
+plt.plot(xs, max_oob_per_iteration[:25], 'o-', color = 'red', label='Random Search')
 plt.plot(xs, y_bo, 'o-', color = 'blue', label='Bayesian Optimization')
 plt.legend()
 plt.xlabel('Iterations')
 plt.ylabel('Out of bag error')
 plt.title('Comparison between Random Search and Bayesian Optimization')
 plt.show()
-
