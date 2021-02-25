e577d1cb344f52a988c3bf4501772c7cd0e990ec
Ne0-1 <57833596+Ne0-1@users.noreply.github.com>
2021-02-25 15:25:51 +0100

    Leave one horse out done <3

--- Project 1 - Bayesian optimization/leave_one_horse_out.py
+++ Project 1 - Bayesian optimization/leave_one_horse_out.py
@@ -1,6 +1,10 @@
 import pandas as pd
-#Split into test and train data
-def split_by_horse(df,horse):
+
+
+# Split into test and train data
+#
+# Takes dataframe as input df and a string as input horse
+def split_by_horse(df, horse):
     test = df[df['horse'].str.match(horse)]
     train = df.drop(df[df['horse'].str.match(horse)].index)
     test = test.reset_index(drop=True)
