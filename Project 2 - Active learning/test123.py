ae88f9d1f0ab6c4b40827ac9c9b98320a430f23f
Yeganeh1377 <59656248+Yeganeh1377@users.noreply.github.com>
2021-03-18 16:06:01 +0100

    new

--- Project 2 - Active learning/test123.py
+++ Project 2 - Active learning/test123.py
@@ -11,5 +11,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-data = datasets.FashionMNIST(root="Project 2 - Active learning")
-print(data)
+#data = datasets.FashionMNIST(root="Project 2 - Active learning", download=True)
+#print(data)
+
+import idx2numpy
+import numpy as np
+file = '/Users/yeganehghamari/Active_ML_and_Agency1/Project 2 - Active learning/data/train-images-idx3-ubyte'
+arr = idx2numpy.convert_from_file(file)
+
+#cv.imshow("Image", arr[4], )
+plt.imshow(arr[4], cmap='gray')
