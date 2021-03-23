0d67ae140a6602b2b6a13c75b58fb57688265ed0
Ne0-1 <57833596+Ne0-1@users.noreply.github.com>
2021-03-23 16:31:05 +0100

    Set seed

--- Project 2 - Active learning/test123.py
+++ Project 2 - Active learning/test123.py
@@ -27,7 +27,7 @@ file_test_label = '/Users/christiandjurhuus/PycharmProjects/Active_ML_and_Agency
 
 #Creating numpy arrays
 arr_train = idx2numpy.convert_from_file(file_train)
-arr_train_label =  idx2numpy.convert_from_file(file_train_label)
+arr_train_label = idx2numpy.convert_from_file(file_train_label)
 arr_test = idx2numpy.convert_from_file(file_test)
 arr_test_label = idx2numpy.convert_from_file(file_test_label)
 
@@ -55,9 +55,10 @@ ypool = ytest[:5000]
 lr = lin.LogisticRegression(penalty='l2',C=1.)
 
 
-addn=2 #samples to add each time
+addn = 2 #samples to add each time
 #randomize order of pool to avoid sampling the same subject sequentially
-order=np.random.permutation(range(len(Xpool)))
+np.random.seed(0)
+order = np.random.permutation(range(len(Xpool)))
 
 #samples in the pool
 poolidx=np.arange(len(Xpool),dtype=np.int)
@@ -73,11 +74,11 @@ model=lr
 testacc=[]
 for i in range(25):
     #Fit model
-    model.fit(Xtrain,ytrain)
+    model.fit(Xtrain, ytrain)
     #predict on test set
-    ye=model.predict(Xtest)
+    ye = model.predict(Xtest)
     #calculate and accuracy and add to list
-    testacc.append((ninit+i*addn,sklearn.metrics.accuracy_score(ytest,ye)))
+    testacc.append((ninit+i*addn, sklearn.metrics.accuracy_score(ytest, ye)))
     print('Model: LR, %i random samples'%(ninit+i*addn))
 
 
