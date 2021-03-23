18c00cca1264fafc855fd9805ef3396ec0796632
ChristianDjurhuus <57835777+ChristianDjurhuus@users.noreply.github.com>
2021-03-23 18:47:58 +0100

    Setting up what should be done

--- Project 2 - Active learning/test123.py
+++ Project 2 - Active learning/test123.py
@@ -1,3 +1,4 @@
+imp
 import torch
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
@@ -6,6 +7,7 @@ import torch.nn as nn
 import torch.optim as optim
 import numpy as np
 from PIL import Image
+import sklearn
 import numpy as np
 import os
 import matplotlib.pyplot as plt
@@ -17,28 +19,18 @@ import idx2numpy
 #data = datasets.FashionMNIST(root="Project 2 - Active learning", download=True)
 #print(data)
 
-#os.chdir('/Users/christiandjurhuus/PycharmProjects/Active_ML_and_Agency/Project 2 - Active learning/Project 2 - Active learning/FashionMNIST/raw')
+os.chdir('/Users/christiandjurhuus/PycharmProjects/Active_ML_and_Agency/Project 2 - Active learning/Project 2 - Active learning/FashionMNIST/raw')
 #Getting file paths
-file_train = '/Users/christiandjurhuus/PycharmProjects/Active_ML_and_Agency/Project 2 - Active learning/Project 2 - Active learning/FashionMNIST/raw/train-images-idx3-ubyte'
-file_test = '/Users/christiandjurhuus/PycharmProjects/Active_ML_and_Agency/Project 2 - Active learning/Project 2 - Active learning/FashionMNIST/raw/t10k-images-idx3-ubyte'
-file_train_label = '/Users/christiandjurhuus/PycharmProjects/Active_ML_and_Agency/Project 2 - Active learning/Project 2 - Active learning/FashionMNIST/raw/train-labels-idx1-ubyte'
-file_test_label = '/Users/christiandjurhuus/PycharmProjects/Active_ML_and_Agency/Project 2 - Active learning/Project 2 - Active learning/FashionMNIST/raw/t10k-labels-idx1-ubyte'
+file_train = 'train-images-idx3-ubyte'
+file_test = 't10k-images-idx3-ubyte'
+file_train_label = 'train-labels-idx1-ubyte'
+file_test_label = 't10k-labels-idx1-ubyte'
 
 #Creating numpy arrays
 arr_train = idx2numpy.convert_from_file(file_train)
 arr_train_label = idx2numpy.convert_from_file(file_train_label)
 arr_test = idx2numpy.convert_from_file(file_test)
 arr_test_label = idx2numpy.convert_from_file(file_test_label)
-
-
-#Due to the size of the dataset we will start by only looking at the test set
-X = arr_test
-y = arr_test_label
-
-
-
-
-
 
 
 
@@ -51,52 +43,95 @@ y = arr_test_label
 #ytrain = arr_train_label[:9000]
 
 
+X = arr_test
+y = arr_test_label
+
 #Creating pools for pool based active learning
-Xtest = np.concatenate(X[5000:]).T
+Xtest = X[5000:].reshape(5000,784)
 ytest = y[5000:]
-Xpool = np.concatenate(X[:5000]).T
+Xpool = X[:5000].reshape(5000,784)
 ypool = y[:5000]
+
+#Visualizing a subsample of the data
+def show_image(x, title="", clim=None, cmap=plt.cm.gray, colorbar=False):
+    ax = plt.gca()
+    im = ax.imshow(x.reshape((28, 28)), cmap=cmap, clim=clim)
+
+    if len(title) > 0:
+        plt.title(title)
+
+    plt.axis('off')
+
+    if colorbar:
+        divider = make_axes_locatable(ax)
+        cax = divider.append_axes("right", size="5%", pad=0.05)
+        plt.colorbar(im, cax=cax)
+
+num_images_per_row = 6
+num_rows = 3
+num_images = num_images_per_row * num_rows
+
+plt.figure(figsize=(20, 8))
+for i in range(num_images):
+    plt.subplot(num_rows, num_images_per_row, 1 + i)
+    show_image(Xtest[i, :])
+plt.show()
+
+
 
 #Defining model
 lr = lin.LogisticRegression(penalty='l2',C=1.)
 
 
+#########################################Randomly increasing the training set##########################################
+
 addn = 2 #samples to add each time
 #randomize order of pool to avoid sampling the same subject sequentially
 np.random.seed(0)
 order = np.random.permutation(range(len(Xpool)))
-
-#samples in the pool
-poolidx=np.arange(len(Xpool),dtype=np.int)
+#samples in the poolx
+poolidx=np.arange(len(Xpool),dtype=int)
 ninit = 10 #initial samples
 #initial training set
 trainset=order[:ninit]
+print(trainset)
 Xtrain=np.take(Xpool,trainset,axis=0)
-ytrain=np.take(ypool,trainset, axis=0)
+ytrain=np.take(ypool,trainset,axis=0)
 #remove data from pool
 poolidx=np.setdiff1d(poolidx,trainset)
 
 model=lr
 testacc=[]
+
 for i in range(25):
     #Fit model
-    model.fit(Xtrain, ytrain)
+    model.fit(np.take(Xpool, order[:ninit+i*addn], axis = 0), np.take(ypool, order[:ninit+i*addn], axis=0))
     #predict on test set
-    ye = model.predict(Xtest)
+    y_hat = model.predict(Xtest)
     #calculate and accuracy and add to list
-    testacc.append((ninit+i*addn, sklearn.metrics.accuracy_score(ytest, ye)))
+    testacc.append((ninit+i*addn, sklearn.metrics.accuracy_score(ytest, y_hat)))
     print('Model: LR, %i random samples'%(ninit+i*addn))
 
+#Plot learning curve (test set on independent subjects)
+plt.plot(*tuple(np.array(testacc).T));
+plt.show()
+
+
+''' 
+Så er der frit slag om hvad man vil implementere nedenunder ;-))
+'''
+
+###############################################Uncertainty sampling#####################################################
+
+
+
+###############################################Query by commitee########################################################
+
+
+
+###############################################Expected model change####################################################
 
 
 
 
-
-
-
-
-
-
-
-
-
+###############################################Variance reduction#######################################################
