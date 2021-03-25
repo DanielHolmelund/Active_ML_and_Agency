2b2f995234d83233facd418086d1352d01ac227c
Ne0-1 <57833596+Ne0-1@users.noreply.github.com>
2021-03-25 15:16:41 +0100

    Uncertainty sampling; least conf margin sample

--- Project 2 - Active learning/Felix.py
+++ Project 2 - Active learning/Felix.py
@@ -19,7 +19,7 @@ import idx2numpy
 #data = datasets.FashionMNIST(root="Project 2 - Active learning", download=True)
 #print(data)
 
-os.chdir('/Users/christiandjurhuus/PycharmProjects/Active_ML_and_Agency/Project 2 - Active learning/Project 2 - Active learning/FashionMNIST/raw')
+os.chdir('/Users/felixburmester/Desktop/AI_Bachelor/Agency/P1/Project 2 - Active learning/Project 2 - Active learning/FashionMNIST/raw')
 #Getting file paths
 file_train = 'train-images-idx3-ubyte'
 file_test = 't10k-images-idx3-ubyte'
@@ -70,31 +70,31 @@ def show_image(x, title="", clim=None, cmap=plt.cm.gray, colorbar=False):
 num_images_per_row = 6
 num_rows = 3
 num_images = num_images_per_row * num_rows
-
+'''
 plt.figure(figsize=(20, 8))
 for i in range(num_images):
     plt.subplot(num_rows, num_images_per_row, 1 + i)
     show_image(Xtest[i, :])
-plt.show()
+plt.show()'''
 
 
 
 #Defining model
-lr = lin.LogisticRegression(penalty='l2',C=1.)
+lr = lin.LogisticRegression(penalty='l2',C=1.,max_iter=5000)
 
 
 #########################################Randomly increasing the training set##########################################
 
-addn = 2 #samples to add each time
+addn = 4 #samples to add each time
 #randomize order of pool to avoid sampling the same subject sequentially
 np.random.seed(0)
 order = np.random.permutation(range(len(Xpool)))
 #samples in the poolx
 poolidx=np.arange(len(Xpool),dtype=int)
-ninit = 10 #initial samples
+ninit = 500 #initial samples
 #initial training set
 trainset=order[:ninit]
-print(trainset)
+
 Xtrain=np.take(Xpool,trainset,axis=0)
 ytrain=np.take(ypool,trainset,axis=0)
 #remove data from pool
@@ -113,8 +113,8 @@ for i in range(25):
     print('Model: LR, %i random samples'%(ninit+i*addn))
 
 #Plot learning curve (test set on independent subjects)
-plt.plot(*tuple(np.array(testacc).T));
-plt.show()
+'''plt.plot(*tuple(np.array(testacc).T));
+plt.show()'''
 
 
 ''' 
@@ -123,6 +123,75 @@ Så er der frit slag om hvad man vil implementere nedenunder ;-))
 
 ###############################################Uncertainty sampling#####################################################
 
+testacc_al = []
+trainset = order[:ninit]
+
+Xtrain = np.take(Xpool, trainset, axis=0)
+ytrain = np.take(ypool, trainset, axis=0)
+poolidx = np.arange(len(Xpool), dtype=int)
+poolidx = np.setdiff1d(poolidx, trainset)
+
+for i in range(25):
+    # fill out code to select samples according to uncertainty here
+    model.fit(Xtrain, ytrain)
+
+    y_hat = model.predict(Xtest)
+
+    testacc_al.append((len(Xtrain), sklearn.metrics.accuracy_score(ytest, y_hat)))
+
+    # Getting label probabilities
+    p = model.predict_proba(Xpool[poolidx])
+    # Sorting the probabilites to find the least confident
+    p_sort = np.argsort(-p.max(1))
+
+    # Now lets add them to the training set and remove them from the pool
+    # adding
+    Xtrain = np.concatenate((Xtrain, Xpool[poolidx[p_sort[-addn:]]]))
+    ytrain = np.concatenate((ytrain, ypool[poolidx[p_sort[-addn:]]]))
+
+    # removing
+    poolidx = np.setdiff1d(poolidx, poolidx[p_sort[-addn:]])
+
+    print('Model: LR, %i random samples' % (len(Xtrain)))
+
+testacc_al_margin = []
+trainset = order[:ninit]
+
+Xtrain = np.take(Xpool, trainset, axis=0)
+ytrain = np.take(ypool, trainset, axis=0)
+poolidx = np.arange(len(Xpool), dtype=int)
+poolidx = np.setdiff1d(poolidx, trainset)
+
+for i in range(25):
+    # fill out code to select samples according to uncertainty here
+    model.fit(Xtrain, ytrain)
+
+    y_hat = model.predict(Xtest)
+
+    testacc_al_margin.append((len(Xtrain), sklearn.metrics.accuracy_score(ytest, y_hat)))
+
+    # Getting label probabilities
+    p = model.predict_proba(Xpool[poolidx])
+    # Sorting the probabilites to find the least confident
+    p_sort = np.argsort(np.sort(p, axis=1)[:,-1] - np.sort(p, axis=1)[:,-2] )
+
+    # Now lets add them to the training set and remove them from the pool
+    # adding
+    Xtrain = np.concatenate((Xtrain, Xpool[poolidx[p_sort[:addn]]]))
+    ytrain = np.concatenate((ytrain, ypool[poolidx[p_sort[:addn]]]))
+
+    # removing
+    poolidx = np.setdiff1d(poolidx, poolidx[p_sort[:addn]])
+
+    print('Model: LR, %i random samples' % (len(Xtrain)))
+print(testacc_al_margin)
+
+#Plot learning curve
+plt.plot(*tuple(np.array(testacc).T))
+plt.plot(*tuple(np.array(testacc_al).T))
+plt.plot(*tuple(np.array(testacc_al_margin).T))
+plt.legend(('random sampling','uncertainty sampling'))
+plt.show()
 
 
 ###############################################Query by commitee########################################################
